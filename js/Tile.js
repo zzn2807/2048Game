50f860fc68f7130f5a4dccabd0b5f10faed2bd40
David Nwokolo <nwokolodz@gmail.com>
2025-06-05 16:48:33 -0400

    Added clarifying comments

--- js/Tile.js
+++ js/Tile.js
@@ -16,25 +16,12 @@ class Tile {
     }
 
     slide(target_space){
-        let newSpace = document.getElementById(target_space.id);
-        let tile = document.getElementById(this.id);
-        let tileRect = tile.getBoundingClientRect();
-        let targetRect = newSpace.getBoundingClientRect();
-        let duration = 200;
-        // tile.animate(
-        //     [
-        //         //keyframes
-        //         {transform: `translate(0px)`},
-        //         {transform: `translate(${targetRect.left - tileRect.left}px,${targetRect.top - tileRect.top}px)`},
-                
-        //     ],
-        //     {
-        //         duration: duration,
-        //         iterations: 1,
-        //     },
-        // );
-        // setTimeout(()=>newSpace.appendChild(tile),duration);
-
+        let newSpace = document.getElementById(target_space.id);//New space html element
+        let tile = document.getElementById(this.id); //This tile html element
+        let tileRect = tile.getBoundingClientRect(); //Tile current coordinates
+        let targetRect = newSpace.getBoundingClientRect();//New Space coordinates
+        let duration = 200;//Sliding duration in ms
+        //Slide to new space physically then place tile under new space in the DOM
         $(tile).animate({
             top: `${targetRect.top - tileRect.top}px`,
             left: `${targetRect.left - tileRect.left}px`
