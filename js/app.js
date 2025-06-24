bc924eb703b739cbd2730ac7f3b87f578a855b2d
David Nwokolo <nwokolodz@gmail.com>
2025-06-24 16:20:08 -0400

    Added gesture control

--- js/app.js
+++ js/app.js
@@ -1,6 +1,53 @@
+let touchstartX;
+let touchstartY;
+let touchendX;
+let touchendY; 
+let touchableElement = document.querySelector('body');
 let game = new Game();
 game.startGame();
 
 document.addEventListener('keydown', function(event){
     game.handleKeyDown(event);
 });
+
+touchableElement.addEventListener('touchstart', function (event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+touchableElement.addEventListener('touchend', function (event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false);
+
+
+function handleGesture() {
+    if (touchendX < touchstartX) {
+        console.log('Swiped Left');
+        let event = {}
+        event.key = "arrowleft"
+        game.handleKeyDown(event)
+    }
+
+    if (touchendX > touchstartX) {
+        console.log('Swiped Right');
+        let event = {}
+        event.key = "arrowright"
+        game.handleKeyDown(event)
+    }
+
+    if (touchendY < touchstartY) {
+        console.log('Swiped Up');
+        let event = {}
+        event.key = "arrowup"
+        game.handleKeyDown(event)
+    }
+
+    if (touchendY > touchstartY) {
+        console.log('Swiped Down');
+        let event = {}
+        event.key = "arrowdown"
+        game.handleKeyDown(event)
+    }
+}
